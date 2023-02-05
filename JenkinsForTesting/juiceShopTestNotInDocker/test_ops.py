a4de5ea3f54f5914d4640118b9b5fb63576e06b6
ilaure <ilaure89@gmail.com>
2023-02-05 20:23:50 +0300

    Update test_ops.py

--- JenkinsForTesting/juiceShopTestNotInDocker/test_ops.py
+++ JenkinsForTesting/juiceShopTestNotInDocker/test_ops.py
@@ -23,6 +23,8 @@ class TestCase(object):
         ("3+5", 8),
         ("2+4", 6),
         ("6*9", 42),
+        ("5*5", 25),
+        ("50-10", 40),
     ])
     def tests_in_testCase(self, test_input, expected):
         assert eval(test_input) == expected
